73365e3bc8cd3db591ba2996b9858105aa4769c9
chenxuehai@alltuu.com <cxh741@qq.com>
2022-07-13 23:50:29 +0800

    feat: monorepo demo finish

---
+++ packages/h5-client/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <el-btn msg="test" @confirm="confirmHandler"></el-btn>
+  </main>
+</template>
+
+<script setup lang="ts">
+// import TheWelcome from "@/components/TheWelcome.vue";
+import { ElBtn } from "@elben/components";
+import { getUserId } from "@elben/apis";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log(getUserId(987123));
+});
+
+const confirmHandler = (msg) => {
+  console.log(msg);
+};
+</script>
